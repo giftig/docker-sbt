9eb81b89cef8fee9a327ad1834bc1b9da1116299
Rob Moore <rob.moore@adstream.com>
2017-03-14 10:22:37 +0000

    Add colour to info header

--- sbt.sh
+++ sbt.sh
@@ -11,10 +11,21 @@ SCALA_VERSION=2.11.6
 VOLUMES="-v $HOME/.ivy2:/root/.ivy2"
 QUIET=false
 
+# Colours
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+
 usage() {
   echo 'docked-sbt --versions'
   echo 'docked-sbt --help'
-  echo 'docked-sbt [--scala VERSION] [--sbt VERSION] [-e|--expose-socket] [--nocache] [--] SBT_OPTIONS'
+  echo 'docked-sbt OPTIONS [--] SBT_OPTIONS'
+  echo ''
+  echo 'Options:'
+  echo -e "--scala VERSION\t\tDefaults to 2.11.6"
+  echo -e "--sbt VERSION\t\tDefaults to 0.13.13"
+  echo -e "-e, --expose-socket\tExpose the host docker.sock to the sbt container"
+  echo -e "--nocache\t\tDon't mount the user's .ivy2 dir as a volume"
+  echo '--nocolor'
 }
 expect_arg() {
   if [[ "$1" == '' ]]; then
@@ -28,6 +39,10 @@ while [[ "$1" != '' ]]; do
     --versions)
       docker images $IMAGE_PREFIX/sbt
       exit 0
+      ;;
+    --nocolor)
+      YELLOW=''
+      RESET=''
       ;;
     --help)
       usage
@@ -76,11 +91,13 @@ if [[ $(docker images -q "$IMAGE") == '' ]]; then
 fi
 
 if [[ "$QUIET" != true ]]; then
+  echo -n $YELLOW
   echo '================================================='
   echo 'DOCKED SBT'
   echo "Using image $IMAGE"
   echo "Using volumes $(echo $VOLUMES | sed 's|-v|\'$'\n-v|g')"
   echo '================================================='
+  echo -n $RESET
 fi
 
 docker run -it --rm -w /usr/src --entrypoint sbt $VOLUMES $IMAGE "$@"
